83f8d36ca7a1397dbf47ae8c0462653dcb86a853
devjorgeluis <jorgencnc0608@gmail.com>
2025-06-05 00:12:00 +0800

    fix import

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,9 @@ import 'flatpickr/dist/flatpickr.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueApexCharts from 'vue3-apexcharts'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(VueApexCharts)
 
 app.mount('#app')
